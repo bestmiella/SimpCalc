73595521a2e0ec1cdda71a80da9e0349e4bf771b
bestmiella <40307669+bestmiella@users.noreply.github.com>
2018-11-08 20:23:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ class SimpPyCalc:
     self.new_num = new_num
     self.reset = reset
     print("""Add, substract, multiply, or divide by entering
-the operator followed by the number
-(Example: enter '+4' to add 4)""")
+    the operator followed by the number
+    (Example: enter '+4' to add 4)""")
 
     def add(curr_num, new_num):
       new_num = float(us_in.lstrip('+'))
@@ -15,29 +15,43 @@ the operator followed by the number
       curr_num = float(curr_num + new_num)
       return curr_num
 
-    def sub():
+    def sub(curr_num, new_num):
+      new_num = float(us_in.lstrip('-'))
       print(curr_num - new_num)
+      curr_num = float(curr_num - new_num)
+      return curr_num
 
-    def mult():
+    def mult(curr_num, new_num):
+      new_num = float(us_in.lstrip('*'))
       print(curr_num * new_num)
+      curr_num = float(curr_num * new_num)
+      return curr_num
 
-    def div():
+    def div(curr_num, new_num):
+      new_num = float(us_in.lstrip('/'))
       print(curr_num / new_num)
+      curr_num = float(curr_num / new_num)
+      return curr_num
     
     def get_input():
-      get_input = input()
+      get_input = input(curr_num)
       return get_input
 
     while reset == False:
       us_in = get_input()
+
       if us_in[0] == "+":
         curr_num = add(curr_num, new_num)
+
       elif us_in[0] == "-":
-        sub()
+        curr_num = sub(curr_num, new_num)
+
       elif us_in[0] == "*":
-        mult()
+        curr_num = mult(curr_num, new_num)
+
       elif us_in[0] == "/":
-        div()
+        curr_num = div(curr_num, new_num)
+
       else:
         print("Error: Invalid. A valid operator would be '+', '-', '*', or '/'")
 
