d2f579c2fc983a86deeecc2d9eb4f51e509cdf1c
bestmiella <40307669+bestmiella@users.noreply.github.com>
2018-11-08 19:13:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 #Simple Python Calculator
 class SimpPyCalc:
 
-  def __init__(self, curr_num, op, reset):
+  def __init__(self, curr_num, new_num, reset):
     self.curr_num = curr_num
-    self.op = op
+    self.new_num = new_num
     self.reset = reset
+    print("""add, substract, multiply, or divide by entering
+the operator followed by the number
+(Example: enter '+4' to add 4)""")
+
+    def add():
+      return curr_num + new_num
+
+    def sub():
+      return curr_num - new_num
+
+    def mult():
+      return curr_num * new_num
+
+    def div():
+      return curr_num / new_num
+    
+    def get_input():
+      get_input = input()
+      return get_input
 
     while reset == False:
-      get_input = SimpPyCalc.get_input()
+      us_in = get_input()
+      if us_in[0] == "+":
+        add()
+      elif us_in[0] == "-":
+        sub()
+      elif us_in[0] == "*":
+        mult()
+      elif us_in[0] == "/":
+        div()
+      else:
+        print("Error: Invalid. A valid operator would be '+', '-', '*', or '/'")
 
-  def add():
-    return 0
-
-  def sub():
-    return 0
-
-  def mult():
-    return 0
-
-  def div():
-    return 0
-  
-  def get_input():
-    us_in = input("""add, substract, multiply, or divide by entering
-the opperator followed by the number
-(Example: enter '+4' to add 4)""")
-    return us_in
-
-calc_session = SimpPyCalc(0, "none", False)
-
-
-
+calc_session = SimpPyCalc(0, 0, False)
