98d8cc7b4fdfa7f1f637a24e93b320a463dcfcfe
bestmiella <40307669+bestmiella@users.noreply.github.com>
2018-11-08 20:09:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ class SimpPyCalc:
     self.curr_num = curr_num
     self.new_num = new_num
     self.reset = reset
-    print("""add, substract, multiply, or divide by entering
+    print("""Add, substract, multiply, or divide by entering
 the operator followed by the number
 (Example: enter '+4' to add 4)""")
 
-    def add():
-      return curr_num + new_num
+    def add(curr_num, new_num):
+      new_num = float(us_in.lstrip('+'))
+      print(curr_num + new_num)
+      curr_num = float(curr_num + new_num)
+      return curr_num
 
     def sub():
-      return curr_num - new_num
+      print(curr_num - new_num)
 
     def mult():
-      return curr_num * new_num
+      print(curr_num * new_num)
 
     def div():
-      return curr_num / new_num
+      print(curr_num / new_num)
     
     def get_input():
       get_input = input()
@@ -28,7 +31,7 @@ the operator followed by the number
     while reset == False:
       us_in = get_input()
       if us_in[0] == "+":
-        add()
+        curr_num = add(curr_num, new_num)
       elif us_in[0] == "-":
         sub()
       elif us_in[0] == "*":
